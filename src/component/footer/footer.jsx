ceef434275547463b967a0a245b50d8b6ce3dce8
Chinmay <ichinmay2461@gmail.com>
2024-11-17 23:06:32 +0530

    footer setup

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <main className="bg-yellow-300 h-52 ">
       <div className="flex items-center justify-center"></div>
-      <left>
-        <ul className="flex justify-around mt-7">
+      <left className='flex gap-5 justify-around'>
+        <ul className="flex justify-around mt-7  w-[70%] ">
           <li>
             <div className="title-style">Contact & e-mail</div>
             <span>+91 7903435992</span>
@@ -45,42 +45,54 @@ const Footer = () => {
               </div>
             ))}
           </li>
+          <li>
+            <h1 className="title-style ">YouTube Channels</h1>
+            <div className="flex items-center space-x-2">
+              <img
+                src="./images/cavlogs.png"
+                alt="CA Vlogs"
+                className="icon-style"
+              />
+              <a
+                href="https://www.youtube.com/@CAVlogsLovesYouAlways"
+                className="text-black hover:underline"
+              >
+                CA Vlogs
+              </a>
+            </div>
 
+            <div className="flex items-center space-x-2">
+              <img
+                src="./images/sas.png"
+                alt="SAS Videos"
+                className="icon-style"
+              />
+              <a
+                href="https://www.youtube.com/@SASVideos"
+                className="text-black hover:underline"
+              >
+                SAS Videos
+              </a>
+            </div>
+          </li>
           <li>
             <h1 className="title-style">copyright</h1>
             <p class=" text-black text-sm">
               &copy; {new Date().getFullYear()} <br /> All rights reserved.
-              <br /> https://portfolio-with-react-brown.vercel.app/
+              <br /> <a href="https://portfolio-with-react-brown.vercel.app/">https://portfolio-with-react-brown.vercel.app/</a> 
             </p>
           </li>
-          
+        </ul>
+
+        <ul>
           <li>
-            <h1 className="title-style ">YouTube Channels</h1>
-              <div className="flex items-center space-x-2">
-                <img src="./images/cavlogs.png" alt="CA Vlogs" className="icon-style"/>
-                <a href="https://www.youtube.com/@CAVlogsLovesYouAlways" className="text-black hover:underline">
-                  CA Vlogs
-                </a>
-              </div>
-
-              <div className="flex items-center space-x-2">
-                <img src="./images/sas.png" alt="SAS Videos" className="icon-style"/>
-                <a href="https://www.youtube.com/@SASVideos" className="text-black hover:underline">
-                  SAS Videos
-                </a>
-              </div>
-            </li>
-
-            <li>
-              <h1 className="font-bold mt-14 border border-black rounded-2xl p-3 shadow-black shadow-2xl bg-black text-yellow-300">
-                ThankYou For Visiting
-              </h1>
-            </li>
-          
+            <h1 className="font-bold mt-20 mr-10 border border-black rounded-2xl p-3 shadow-black shadow-2xl bg-black text-yellow-300">
+              ThankYou For Visiting
+            </h1>
+          </li>
         </ul>
       </left>
     </main>
   );
 };
-
 export default Footer;
